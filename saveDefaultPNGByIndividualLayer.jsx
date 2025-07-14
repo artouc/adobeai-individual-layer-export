872aaa0ab70e6d3ced39fcfb84961754dd96abf5
artouc <arataouchi@outlook.com>
2025-07-14 14:01:58 +0900

    feat: スクリプト公開

---
+++ saveDefaultPNGByIndividualLayer.jsx
@@ -0,0 +1,37 @@
+const folder = Folder.selectDialog("エクスポート先のフォルダを選択してください");
+const document = app.activeDocument;
+
+if(document && folder) {
+    const options = new ExportOptionsPNG24();
+    options.antiAliasing = true;
+    options.transparency = true;
+    options.artBoardClipping = true;
+    const length = document.layers.length;
+    for(var i = 0; i < length; ++i) {
+        hideAllLayers();
+        var layer = document.layers[i];
+        layer.visible = true;
+        var file = new File(folder.fsName+"/"+layer.name+".png");
+        document.exportFile(file,ExportType.PNG24,options);
+    }
+    showAllLayers();
+}
+
+function hideAllLayers() {
+    forEach(document.layers, function(layer) {
+        layer.visible = false;
+    });
+}
+
+function showAllLayers() {
+    forEach(document.layers, function(layer) {
+        layer.visible = true;
+    });
+}
+
+function forEach(collection, fn) {
+    const length = collection.length;
+    for(var i = 0; i < length; ++i) {
+        fn(collection[i]);
+    }
+}
